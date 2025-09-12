30490e3a7c18b6479a393074eac77d90624c398d
cp4001 <46122970+cp4001@users.noreply.github.com>
2025-09-12 12:54:28 +0900

    .csproj  수정

---
+++ Acadv25JArch/Commands.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acadv25JArch
+{
+    class Commands
+    {
+
+        [CommandMethod("Show_BlockForm")]
+        public void ShowMyForm()
+        {
+            FormBlockPart  form = new FormBlockPart();
+            Autodesk.AutoCAD.ApplicationServices.Application.ShowModalDialog(form);
+            //form.ShowDialog();
+        }
+    }
+}
