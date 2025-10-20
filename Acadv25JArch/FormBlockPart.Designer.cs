f6785c4a3c448c0b7a930d7a395137842fed4807
cp4001 <46122970+cp4001@users.noreply.github.com>
2025-10-20 12:41:16 +0900

    Room Tab 설정

--- Acadv25JArch/FormBlockPart.Designer.cs
+++ Acadv25JArch/FormBlockPart.Designer.cs
@@ -29,14 +29,24 @@
         private void InitializeComponent()
         {
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             btnAllBlocks = new Button();
             dgvBlock = new DataGridView();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            btnRooms = new Button();
+            dgvRoom = new DataGridView();
             ((System.ComponentModel.ISupportInitialize)dgvBlock).BeginInit();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvRoom).BeginInit();
             SuspendLayout();
             // 
             // btnAllBlocks
             // 
-            btnAllBlocks.Location = new Point(67, 45);
+            btnAllBlocks.Location = new Point(23, 27);
             btnAllBlocks.Name = "btnAllBlocks";
             btnAllBlocks.Size = new Size(112, 34);
             btnAllBlocks.TabIndex = 0;
@@ -55,23 +65,88 @@
             dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
             dgvBlock.DefaultCellStyle = dataGridViewCellStyle1;
-            dgvBlock.Location = new Point(163, 120);
+            dgvBlock.Location = new Point(90, 108);
             dgvBlock.Name = "dgvBlock";
             dgvBlock.RowHeadersWidth = 62;
             dgvBlock.RowTemplate.Height = 50;
-            dgvBlock.Size = new Size(1084, 464);
+            dgvBlock.Size = new Size(1164, 428);
             dgvBlock.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1376, 690);
+            tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(dgvBlock);
+            tabPage1.Controls.Add(btnAllBlocks);
+            tabPage1.Location = new Point(4, 34);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1368, 652);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Blocks";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(dgvRoom);
+            tabPage2.Controls.Add(btnRooms);
+            tabPage2.Location = new Point(4, 34);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1368, 652);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Rooms";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnRooms
+            // 
+            btnRooms.Location = new Point(33, 31);
+            btnRooms.Name = "btnRooms";
+            btnRooms.Size = new Size(112, 34);
+            btnRooms.TabIndex = 1;
+            btnRooms.Text = "All Rooms";
+            btnRooms.UseVisualStyleBackColor = true;
+            // 
+            // dgvRoom
+            // 
+            dgvRoom.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("맑은 고딕", 14F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dgvRoom.DefaultCellStyle = dataGridViewCellStyle2;
+            dgvRoom.Location = new Point(68, 93);
+            dgvRoom.Name = "dgvRoom";
+            dgvRoom.RowHeadersWidth = 62;
+            dgvRoom.RowTemplate.Height = 50;
+            dgvRoom.Size = new Size(1164, 428);
+            dgvRoom.TabIndex = 2;
             // 
             // FormBlockPart
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1376, 690);
-            Controls.Add(dgvBlock);
-            Controls.Add(btnAllBlocks);
+            Controls.Add(tabControl1);
             Name = "FormBlockPart";
             Text = "BlocksForm";
             ((System.ComponentModel.ISupportInitialize)dgvBlock).EndInit();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvRoom).EndInit();
             ResumeLayout(false);
         }
 
@@ -79,5 +154,10 @@
 
         private Button btnAllBlocks;
         private DataGridView dgvBlock;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private DataGridView dgvRoom;
+        private Button btnRooms;
     }
 }
